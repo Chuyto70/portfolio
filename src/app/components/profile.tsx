e7fd84b9a7b08b20c70411937bc1328dbaae08b8
Jesus Hurtado <jjhurtado016@gmail.com>
2025-05-07 14:09:16 -0400

    adding some images and new profile image

--- src/app/components/profile.tsx
+++ src/app/components/profile.tsx
@@ -1,32 +1,42 @@
 import Image from "next/image";
-import { IoIosMail, IoMdCall  } from "react-icons/io";
-export default function Profile(){
-    return (
-        <div className="bg-white dark:bg-[#374151] rounded-xl p-4 shadow-md flex flex-col items-center">
-            <div className="flex items-center">
-                <Image
-                className="rounded-xl"
-                alt="Imagen de perfil"
-                width={320}
-                height={292}
-                priority
-                src={'/perfil.jpeg'} />
-            </div>
-            <h3 className="font-semibold text-[#4F4F4F] dark:text-white mt-3">Jesús Hurtado 👋</h3>
-            <span className="text-xs text-[#828282] dark:text-white font-medium mb-6">Full-Stack Developer</span>
+import { IoIosMail, IoMdCall } from "react-icons/io";
+export default function Profile() {
+  return (
+    <div className="bg-white dark:bg-[#374151] rounded-xl p-4 shadow-md flex flex-col items-center">
+      <div className="flex items-center">
+        <Image
+          className="rounded-xl"
+          alt="Imagen de perfil"
+          width={320}
+          height={292}
+          priority
+          src={"/profile_image_ofuscade.png"}
+        />
+      </div>
+      <h3 className="font-semibold text-[#4F4F4F] dark:text-white mt-3">
+        Jesús Hurtado 👋
+      </h3>
+      <span className="text-xs text-[#828282] dark:text-white font-medium mb-6">
+        Full-Stack Developer
+      </span>
 
-            <div className="flex mt-6 items-center gap-2">
-                <IoIosMail size={24} className="dark:text-white " />
-                <span className="text-[#4F4F4F] dark:text-white font-medium">jjhurtado016@gmail.com</span>
-            </div>
-            <div className="flex mt-2 items-center gap-2">
-                <IoMdCall size={24} className="dark:text-white " />
-                <span className="text-[#4F4F4F] dark:text-white font-medium">(+58) 414 952 3016</span>
-            </div>
+      <div className="flex mt-6 items-center gap-2">
+        <IoIosMail size={24} className="dark:text-white " />
+        <span className="text-[#4F4F4F] dark:text-white font-medium">
+          jjhurtado016@gmail.com
+        </span>
+      </div>
+      <div className="flex mt-2 items-center gap-2">
+        <IoMdCall size={24} className="dark:text-white " />
+        <span className="text-[#4F4F4F] dark:text-white font-medium">
+          (+58) 414 952 3016
+        </span>
+      </div>
 
-            <p className="text-[#828282] dark:text-white mt-4 text-center">
-                Desarrollador motivado y dispuesto a aprender y crear aplicaciones de interfaz de usuario excepcionales o del lado del servidor.
-            </p>
-        </div>
-    )
+      <p className="text-[#828282] dark:text-white mt-4 text-center">
+        Desarrollador motivado y dispuesto a aprender y crear aplicaciones de
+        interfaz de usuario excepcionales o del lado del servidor.
+      </p>
+    </div>
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,40 +1,60 @@
-import type { Metadata } from 'next'
+import type { Metadata } from "next";
 
-import './globals.css'
-import { MainContainer } from '@/src/app/components/MainContainer';
-
-
-
+import "./globals.css";
+import { MainContainer } from "@/src/app/components/MainContainer";
 
 export const metadata: Metadata = {
-  title: 'Portafolio - Jesús Hurtado',
-  description: 'Soy Jesús Hurtado, desarrollador web con pasión por la tecnología y la creación de experiencias digitales efectivas.',
-  keywords: ['Jesús Hurtado', 'desarrollador web', 'portafolio', 'frontend', 'JavaScript', 'React', 'Next.js'],
-  authors: [{ name: 'Jesús Hurtado', url: 'https://www.linkedin.com/in/jesus-hurtado-alvarez-290670168' }],
-  creator: 'Jesús Hurtado',
-  metadataBase: new URL('https://jesushurtado.vercel.app'),
+  title: "Portafolio - Jesús Hurtado",
+  description:
+    "Soy Jesús Hurtado, desarrollador web con pasión por la tecnología y la creación de experiencias digitales efectivas.",
+  keywords: [
+    "Jesús Hurtado",
+    "desarrollador web",
+    "programador",
+    "programación",
+    "portafolio",
+    "frontend",
+    "JavaScript",
+    "React",
+    "Next.js",
+    "web",
+    "web development",
+  ],
+  authors: [
+    {
+      name: "Jesús Hurtado",
+      url: "https://www.linkedin.com/in/jesus-hurtado-alvarez-290670168",
+    },
+  ],
+  creator: "Jesús Hurtado",
+  metadataBase: new URL("https://jesushurtado.vercel.app"),
   openGraph: {
-    title: 'Portafolio - Jesús Hurtado',
-    description: 'Portafolio de Jesús Hurtado, desarrollador web con experiencia en React y Next.js.',
-    url: 'https://jesushurtado.vercel.app',
-    siteName: 'Jesús Hurtado',
-    locale: 'es_ES',
-    type: 'website',
+    title: "Portafolio - Jesús Hurtado",
+    description:
+      "Portafolio de Jesús Hurtado, desarrollador web con experiencia en React y Next.js.",
+    url: "https://jesushurtado.vercel.app",
+    siteName: "Jesús Hurtado",
+    locale: "es_ES",
+    type: "website",
+    images: [
+      {
+        url: "https://jesushurtado.vercel.app/logo-removebg-preview.png", // Ruta absoluta
+        width: 500,
+        height: 500,
+        alt: "Jesús Hurtado - Desarrollador Web",
+      },
+    ],
   },
   robots: {
     index: true,
     follow: true,
   },
-}
+};
 
 export default function RootLayout({
-  children
+  children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  return (
-    <MainContainer >
-      {children}
-    </MainContainer>
-  )
+  return <MainContainer>{children}</MainContainer>;
 }
